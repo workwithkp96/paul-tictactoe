6cd267c0117151a0b75197897a64fb9a44b4ff38
Karan Paul <workwithkp96@gmail.com>
2024-08-02 20:02:06 +0530

    class diagram changes & domain classes

--- src/main/java/in/paul/tictactoe/domain/Board.java
+++ src/main/java/in/paul/tictactoe/domain/Board.java
@@ -1,9 +1,7 @@
 package in.paul.tictactoe.domain;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
 public class Board {
 }
---
+++ src/main/java/in/paul/tictactoe/domain/BotLevel.java
@@ -0,0 +1,4 @@
+package in.paul.tictactoe.domain;
+
+public enum BotLevel {
+}
