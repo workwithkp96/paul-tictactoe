4f310860e2d589eacf18137869691e90a85b0879
Karan Paul <workwithkp96@gmail.com>
2024-08-04 00:20:41 +0530

    defining const and board functions

--- src/main/java/in/paul/tictactoe/domain/Board.java
+++ src/main/java/in/paul/tictactoe/domain/Board.java
@@ -1,10 +1,36 @@
 package in.paul.tictactoe.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class Board {
     List<List<Cell>> grid;
+
+    public Board(){
+        createGrid();
+    }
+
+    public void displayBoard(){
+        for(List<Cell> row : grid){
+            for(Cell cell : row){
+                System.out.print("| " + cell.getSymbol().sign + " |");
+            }
+            System.out.println();
+        }
+    }
+
+    public void createGrid(){
+        for(int i=0;i<3;i++){
+            List<Cell> row = new ArrayList<>();
+            for(int j=0;j<3;j++){
+                row.add(new Cell(i,j,null,CellState.EMPTY));
+            }
+            grid.add(row);
+        }
+    }
 }
